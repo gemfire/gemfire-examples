f63ff5a13fd50cc52a22dc95b6f661d60b26ca7f
Jianxia Chen <11181423+jchen21@users.noreply.github.com>
2022-10-31 12:31:25 -0700

    Address some more review comments

--- json/src/main/java/org/apache/geode_examples/json/Example.java
+++ json/src/main/java/org/apache/geode_examples/json/Example.java
@@ -1,20 +1,5 @@
 // Copyright (c) VMware, Inc. 2022.
 // All rights reserved. SPDX-License-Identifier: Apache-2.0
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
- * agreements. See the NOTICE file distributed with this work for additional information regarding
- * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License. You may obtain a
- * copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- */
 package org.apache.geode_examples.json;
 
 import java.util.List;
@@ -28,13 +13,15 @@ import org.apache.geode.cache.query.NameResolutionException;
 import org.apache.geode.cache.query.QueryInvocationTargetException;
 import org.apache.geode.cache.query.TypeMismatchException;
 import org.apache.geode.json.JsonDocument;
+import org.apache.geode.json.JsonDocumentFactory;
 import org.apache.geode.json.JsonParseException;
 
 public class Example {
 
   private static final int NUM_KEYS = 10;
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FunctionDomainException, TypeMismatchException,
+      QueryInvocationTargetException, NameResolutionException {
     // connect to the locator using default port 10334
     ClientCache cache = new ClientCacheFactory().addPoolLocator("127.0.0.1", 10334)
         .set("log-level", "WARN").create();
@@ -43,12 +30,12 @@ public class Example {
     Region<Long, JsonDocument> region =
         cache.<Long, JsonDocument>createClientRegionFactory(ClientRegionShortcut.PROXY)
             .create("example-region");
-
+    // The default is StorageFormat.BSON for cache.getJsonDocumentFactory()
+    // If you prefer StorageFormat.PDX, use cache.getJsonDocumentFactory(StorageFormat.PDX)
+    JsonDocumentFactory jsonDocumentFactory = cache.getJsonDocumentFactory();
     for (long i = 0; i < NUM_KEYS; i++) {
       try {
-        // The default is StorageFormat.BSON.
-        // If you prefer StorageFormat.PDX, use cache.getJsonDocumentFactory(StorageFormat.PDX)
-        JsonDocument jsonDocument = cache.getJsonDocumentFactory().create(createJson(i));
+        JsonDocument jsonDocument = jsonDocumentFactory.create(createJson(i));
         region.put(i, jsonDocument);
       } catch (JsonParseException e) {
         throw new RuntimeException(e);
@@ -57,24 +44,19 @@ public class Example {
 
     JsonDocument jsonDocument = region.get(0L);
     System.out.println("The JSON string is:\n\n" + jsonDocument.toJson());
-    System.out.println("\nUsing Doucment.getField(), the value of JSON intField is "
+    System.out.println("\nUsing Document.getField(), the value of JSON intField is "
         + jsonDocument.getField("intField"));
-    System.out.println("\nUsing Doucment.getField(), the value of JSON nestedField.field2 is "
+    System.out.println("\nUsing Document.getField(), the value of JSON nestedField.field2 is "
         + ((JsonDocument) jsonDocument.getField("nestedField")).getField("field2"));
-    System.out.println("\nUsing Doucment.getField(), the value of JSON arrayField[1] is "
+    System.out.println("\nUsing Document.getField(), the value of JSON arrayField[1] is "
         + ((List) jsonDocument.getField("arrayField")).get(1));
 
-    try {
-      System.out.println("\nQuery: select * from /example-region where name='name5'");
-      System.out.println("\nQuery result:\n\n" + region.getRegionService().getQueryService()
-          .newQuery("select * from /example-region where name='name5'").execute());
-      System.out.println("\nQuery: select * from /example-region where arrayField[0]=5");
-      System.out.println("\nQuery result:\n\n" + region.getRegionService().getQueryService()
-          .newQuery("select * from /example-region where arrayField[0]=5").execute());
-    } catch (FunctionDomainException | TypeMismatchException | NameResolutionException
-        | QueryInvocationTargetException e) {
-      throw new RuntimeException(e);
-    }
+    System.out.println("\nQuery: select * from /example-region where name='name5'");
+    System.out.println("\nQuery result:\n\n" + cache.getQueryService()
+        .newQuery("select * from /example-region where name='name5'").execute());
+    System.out.println("\nQuery: select * from /example-region where arrayField[0]=5");
+    System.out.println("\nQuery result:\n\n" + cache.getQueryService()
+        .newQuery("select * from /example-region where arrayField[0]=5").execute());
     cache.close();
   }
 
