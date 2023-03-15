ce1cbdd3c05f90719a5b221dcda74648cacb9c50
Jacob Barrett <jabarrett@vmware.com>
2023-03-15 10:34:43 -0700

    Adds ServerLauncher and LocatorLauncher example.

---
+++ feature-examples/launchers/locator/scripts/start-gemfire-9.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#Copyright (c) VMware, Inc. 2023. All rights reserved.
+#SPDX-License-Identifier: Apache-2.0
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [ -z "${GEODE_HOME}" ]; then
+  echo "Environment GEODE_HOME not set."
+  exit 1
+fi
+
+# Find the GemFire dDependencies Jar file
+GEMFIRE_DEPENDENCIES="${GEODE_HOME}"/lib/geode-dependencies.jar
+if [ ! -e "${GEMFIRE_DEPENDENCIES}" ]; then
+  echo "Environment GEODE_HOME does not reference a GemFire installation."
+  exit 1
+fi
+
+# Run application directly with GemFire Dependencies on the classpath
+java -classpath "${GEMFIRE_DEPENDENCIES}":"${SCRIPT_DIR}"/../build/classes/java/main \
+  com.vmware.gemfire.examples.launchers.ExampleLocatorApplication \
+  "Hello world!"
---
+++ feature-examples/launchers/server/scripts/start.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#Copyright (c) VMware, Inc. 2023. All rights reserved.
+#SPDX-License-Identifier: Apache-2.0
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [ -z "${GEMFIRE_HOME}" ]; then
+  echo "Environment GEMFIRE_HOME not set."
+  exit 1
+fi
+
+# Find the GemFire Bootstrap Jar file
+GEMFIRE_BOOTSTRAP=("${GEMFIRE_HOME}"/lib/gemfire-bootstrap-*.jar)
+if [ ! -e "${GEMFIRE_BOOTSTRAP[0]}" ]; then
+  echo "Environment GEMFIRE_HOME does not reference a GemFire installation."
+  exit 1
+fi
+
+# Run application via GemFire Bootstrap (com.vmware.gemfire.bootstrap.Main)
+java -classpath "${GEMFIRE_BOOTSTRAP[0]}" \
+  com.vmware.gemfire.bootstrap.Main \
+  com.vmware.gemfire.examples.launchers.ExampleServerApplication \
+  --automatic-module-classpath "${SCRIPT_DIR}"/../build/classes/java/main \
+  "Hello world!"
