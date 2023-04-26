3873bc667f5e9d611d48f677e7a1e01ee24da956
Paul Nikonowicz <pnikonowicz@vmware.com>
2023-04-26 15:38:31 -0400

    squash: addresses various PR comments

--- quickstart/inline-caching/cache-loader/src/main/java/io/vmware/event/ItemCacheLoader.java
+++ quickstart/inline-caching/cache-loader/src/main/java/io/vmware/event/ItemCacheLoader.java
@@ -3,6 +3,7 @@ package io.vmware.event;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import org.jooq.DSLContext;
 import org.jooq.Record;
@@ -11,6 +12,7 @@ import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 
 import org.apache.geode.LogWriter;
+import org.apache.geode.cache.Cache;
 import org.apache.geode.cache.CacheFactory;
 import org.apache.geode.cache.CacheLoader;
 import org.apache.geode.cache.LoaderHelper;
@@ -24,15 +26,22 @@ public class ItemCacheLoader implements CacheLoader<String, String> {
 		log = CacheFactory.getAnyInstance().getDistributedSystem().getLogWriter();
 	}
 
-	public String load(LoaderHelper helper) {
-		int itemId = Integer.parseInt((String) helper.getKey());
-		String userName = System.getProperty("postgres.username");
-		String password = System.getProperty("postgres.password");
+	private String userName;
+	private String password;
+
+	@Override
+	public void initialize(Cache cache, Properties properties) {
+		userName = System.getProperty("postgres.username");
+		password = System.getProperty("postgres.password");
+		log.info("username: "+ userName);
+		log.info("password: "+ password);
+	}
+
+	public String load(LoaderHelper<String, String> helper) {
+		int itemId = Integer.parseInt(helper.getKey());
 		String url = "jdbc:postgresql:postgres";
 		log.info("Cache miss... Loading data from postgres...");
 		log.info("itemId: "+itemId);
-		log.info("username: "+ userName);
-		log.info("password: "+ password);
 		try {
 			log.info("driver:" + Class.forName("org.postgresql.Driver"));
 		} catch (ClassNotFoundException e) {
@@ -44,7 +53,7 @@ public class ItemCacheLoader implements CacheLoader<String, String> {
 			DSLContext create = DSL.using(conn, SQLDialect.POSTGRES);
 			Result<Record> result = create.fetch("select filler from pgbench_tellers where tid=?", itemId);
 
-			return result.isEmpty() ? "NOT FOUND" : (String) result.getValue(0, 0);
+			return result.isEmpty() ? null : (String) result.getValue(0, 0);
 		} catch (SQLException e) {
 			log.error("Exception", e);
 			throw new RuntimeException(e);
