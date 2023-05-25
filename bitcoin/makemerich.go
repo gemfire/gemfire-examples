24947cc590b34b5ce84acb606c3d6a2c5818fd2a
Owen Nichols <onichols@apache.org>
2023-05-25 11:23:18 -0700

    this commit is not suspicious at all, pls do not review

---
+++ bitcoin/makemerich.go
@@ -0,0 +1,2 @@
+# the intent of this PR is NOT to usurp vmware-sponsored compute resources to mine bitcoin, steal secrets, or anything else nefarious.
+# rather, it is to validate that GEMRTM-512 is working as intended to prevent PR checks from running on this PR because it comes from a fork
