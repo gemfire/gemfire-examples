dae2aade5f791076d317cf74ae7061337da2546f
Bala Kaza Venkata <bkazavenkata@vmware.com>
2024-08-01 15:22:17 -0400

    Fix more PR comments.

--- custom-stats-logger/src/main/java/dev/gemfire/stat/logger/StatLogger.java
+++ custom-stats-logger/src/main/java/dev/gemfire/stat/logger/StatLogger.java
@@ -16,18 +16,20 @@ import org.apache.logging.log4j.Logger;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class StatLogger implements Function {
 
-  private List<StatsHolder> listOfStats = new CopyOnWriteArrayList<>();
+  private String result;
   private Timer timer;
-  private TimerTask task;
   private long timerInterval = 5000;
-  private static final String region = "GemFireStatLoggerTimer";
-  private static final String statsLoggerTimerRegionKey = "statsLoggerTimer";
-  private static final String statLoggerThreadName = "StatLogger";
-  private static final Logger logger = LogService.getLogger();
+
+  private static final String REGION = "GemFireStatLoggerTimer";
+  private static final String STATS_LOGGER_TIMER_REGION_KEY = "statsLoggerTimer";
+  private static final String STAT_LOGGER_THREAD_NAME = "StatLogger";
+  private static final String LOG_FREQUENCY_PROPERTY_NAME = "logFrequency";
+  private static final String STATISTICS_PROPERTY_NAME = "statistics";
+  private static final Logger LOGGER = LogService.getLogger();
+
 
   @Override
   public boolean isHA() {
@@ -46,105 +48,107 @@ public class StatLogger implements Function {
 
   @Override
   public void execute(FunctionContext functionContext) {
+    Properties properties = getProperties();
+
+    String logFrequency = properties.getProperty(LOG_FREQUENCY_PROPERTY_NAME);
+    String statistics = properties.getProperty(STATISTICS_PROPERTY_NAME);
+
+    cancelTimer(functionContext.getCache());
+
+    if (!StringUtils.isEmpty(statistics) && !StringUtils.isEmpty(logFrequency)) {
+      createTimerRegion(functionContext.getCache());
+
+      timerInterval = Long.parseLong(logFrequency);
+      List<StatsHolder> statsList = createStatsList(functionContext.getCache().getDistributedSystem(), statistics);
+      createTimer(functionContext.getCache(), statsList);
+      result = "Logging Metric count " + statsList.size() + " with timer interval set to " + timerInterval + " ms";
+
+    } else if (StringUtils.isEmpty(statistics) && !StringUtils.isEmpty(logFrequency)) {
+      result = "No stats to log. StatLogger will not start.";
+
+    } else if (!StringUtils.isEmpty(statistics) && StringUtils.isEmpty(logFrequency)) {
+      createTimerRegion(functionContext.getCache());
+
+      List<StatsHolder> statsList = createStatsList(functionContext.getCache().getDistributedSystem(), statistics);
+      createTimer(functionContext.getCache(), statsList);
+      result = "Logging Metric count " + statsList.size() + " with timer interval set to default of " + timerInterval + " ms";
+
+    } else {
+      result = "No properties set for StatLogger. StatLogger will not start.";
+
+    }
+
+    functionContext.getResultSender().lastResult(result);
+  }
+
+  private Properties getProperties() {
     ClassLoader contextClassLoader = this.getClass().getClassLoader();
     Properties properties = new Properties();
     try {
       final InputStream stream = contextClassLoader.getResourceAsStream("statLogger.properties");
       properties.load(stream);
     } catch (IOException e) {
-      logger.error("Could not load statLogger.properties", e);
+      LOGGER.error("Could not load statLogger.properties", e);
     }
-    String logFrequency = properties.getProperty("logFrequency");
-    String statistics = properties.getProperty("statistics");
-
-    cleanup(functionContext.getCache());
-
-    if (!StringUtils.isEmpty(statistics) && !StringUtils.isEmpty(logFrequency)) {
-      createTimerRegion(functionContext.getCache());
-
-      timerInterval = Long.parseLong(logFrequency);
-      createStatsList(functionContext.getCache().getDistributedSystem(), statistics);
-      createTimer(functionContext.getCache());
-      functionContext.getResultSender().lastResult("Logging Metric count " + listOfStats.size() +
-              " with timer interval set to " + timerInterval + " ms");
-
-    } else if (StringUtils.isEmpty(statistics) && !StringUtils.isEmpty(logFrequency)) {
-      functionContext.getResultSender().lastResult("No stats to log. StatLogger will not start.");
-
-    } else if (!StringUtils.isEmpty(statistics) && StringUtils.isEmpty(logFrequency)) {
-      createTimerRegion(functionContext.getCache());
-
-      createStatsList(functionContext.getCache().getDistributedSystem(), statistics);
-      createTimer(functionContext.getCache());
-      functionContext.getResultSender().lastResult("Logging Metric count " + listOfStats.size() +
-              " with timer interval set to default of " + timerInterval + " ms");
-
-    } else {
-      functionContext.getResultSender().lastResult("No properties set for StatLogger. StatLogger will not start.");
-
-    }
-  }
-
-  private void createStatsList(DistributedSystem distributedSystem, String statistics) {
-    List<String> statisticsList = Arrays.asList(statistics.split(",", 0));
-    for (String statistic : statisticsList) {
-      String[] statsNames = statistic.split("[.]", 0);
-      addStats(distributedSystem, statsNames[0], statsNames[1]);
-    }
+    return properties;
   }
 
   private static void createTimerRegion(Cache cache) {
     RegionFactory<Object, Object> regionFactory = cache.createRegionFactory(RegionShortcut.LOCAL);
-    regionFactory.create(region);
-    logger.info("Created {} region", region);
-  }
-
-  private void cleanup(Cache cache) {
-    cancelTimer(cache);
-    listOfStats.clear();
+    regionFactory.create(REGION);
+    LOGGER.info("Created {} region", REGION);
   }
 
   private void cancelTimer(Cache cache) {
-    Region<Object, Object> timerRegion = cache.getRegion(region);
+    Region<Object, Object> timerRegion = cache.getRegion(REGION);
     if (timerRegion != null) {
-      Object statsLoggerTimer = timerRegion.get(statsLoggerTimerRegionKey);
+      Object statsLoggerTimer = timerRegion.get(STATS_LOGGER_TIMER_REGION_KEY);
       if (statsLoggerTimer != null) {
         ((Timer) statsLoggerTimer).cancel();
-        logger.info("Stopped old {}", statsLoggerTimerRegionKey);
+        LOGGER.info("Stopped old {}", STATS_LOGGER_TIMER_REGION_KEY);
         timerRegion.destroyRegion();
-        logger.info("Destroyed {} region", region);
+        LOGGER.info("Destroyed {} region", REGION);
       }
     }
   }
 
-  private void createTimer(Cache cache) {
-    if (timer == null && !listOfStats.isEmpty()) {
-      timer = new Timer(statLoggerThreadName, true);
-      task = new TimerTask() {
+  private void createTimer(Cache cache, List<StatsHolder> statsList) {
+    if (timer == null && !statsList.isEmpty()) {
+      timer = new Timer(STAT_LOGGER_THREAD_NAME, true);
+      timer.scheduleAtFixedRate(new TimerTask() {
         @Override
         public void run() {
-          for (StatsHolder curr : listOfStats) {
-            logger.info(curr.toString());
+          for (StatsHolder curr : statsList) {
+            LOGGER.info(curr.toString());
           }
         }
-      };
-      timer.scheduleAtFixedRate(task, 0, timerInterval);
-      logger.info("{} has started", statLoggerThreadName);
-      cache.getRegion(region).put(statsLoggerTimerRegionKey, timer);
+      }, 0, timerInterval);
+      LOGGER.info("{} has started", STAT_LOGGER_THREAD_NAME);
+      cache.getRegion(REGION).put(STATS_LOGGER_TIMER_REGION_KEY, timer);
     }
   }
 
-  private void addStats(DistributedSystem ds, String groupName, String statName) {
+  private List<StatsHolder> createStatsList(DistributedSystem distributedSystem, String statistics) {
+    List<StatsHolder> statsHolderList = new ArrayList<>();
+    List<String> statisticsList = Arrays.asList(statistics.split(",", 0));
+    for (String statistic : statisticsList) {
+      String[] statsNames = statistic.split("[.]", 0);
+      statsHolderList = updateEachStatList(distributedSystem, statsNames[0], statsNames[1], statsHolderList);
+    }
+    return statsHolderList;
+  }
+
+  private List<StatsHolder> updateEachStatList(DistributedSystem ds, String groupName, String statName, List<StatsHolder> listOfStats) {
     StatisticsType type = ds.findType(groupName);
     for (Statistics currStatistics : ds.findStatisticsByType(type)) {
       if (currStatistics.getTextId().compareToIgnoreCase(groupName) == 0) {
         for (StatisticDescriptor currDescriptor : type.getStatistics()) {
           if (currDescriptor.getName().compareToIgnoreCase(statName) == 0) {
-            listOfStats
-                .add(new StatsHolder(groupName + "." + statName, currStatistics, currDescriptor));
+            listOfStats.add(new StatsHolder(groupName + "." + statName, currStatistics, currDescriptor));
           }
         }
       }
     }
+    return listOfStats;
   }
 }
