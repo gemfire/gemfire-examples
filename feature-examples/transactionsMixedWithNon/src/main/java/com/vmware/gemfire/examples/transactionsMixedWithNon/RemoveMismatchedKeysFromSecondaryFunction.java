f24d1898b1eb84c3d202e5014639b061452c59c4
Xiaojian Zhou <xiaojian.zhou@broadcom.com>
2025-05-19 11:21:29 -0700

    add RemoveMismatchedKeysFromSecondaryFunction

---
+++ feature-examples/transactionsMixedWithNon/src/main/java/com/vmware/gemfire/examples/transactionsMixedWithNon/RemoveMismatchedKeysFromSecondaryFunction.java
@@ -0,0 +1,33 @@
+// Copyright (c) VMware, Inc. 2025.
+// All rights reserved. SPDX-License-Identifier: Apache-2.0
+
+package com.vmware.gemfire.examples.transactionsMixedWithNon;
+
+import org.apache.geode.cache.execute.Function;
+import org.apache.geode.cache.execute.FunctionContext;
+import org.apache.geode.internal.cache.BucketRegion;
+import org.apache.geode.internal.cache.PartitionedRegion;
+
+public class RemoveMismatchedKeysFromSecondaryFunction implements Function<Object[]> {
+  @Override
+  public void execute(FunctionContext<Object[]> context) {
+    final Object[] args = context.getArguments();
+    String regionName = (String) args[0];
+    String mismatchedKey = (String) args[1];
+    PartitionedRegion pr = (PartitionedRegion) context.getCache().getRegion(regionName);
+
+    // System.out.println("In RemoveMismatchedKeysFromSecondaryFunction:" + args[0]+":"+args[1]);
+    for (int bucketId : pr.getDataStore().getAllLocalBucketIds()) {
+      BucketRegion br = pr.getDataStore().getLocalBucketById(bucketId);
+      if (br.containsKey(mismatchedKey)) {
+        br.destroyRecoveredEntry(mismatchedKey);
+      }
+    }
+    context.getResultSender().lastResult("OK");
+  }
+
+  @Override
+  public boolean isHA() {
+    return false;
+  }
+}
